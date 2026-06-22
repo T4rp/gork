3a83b0061f833f285c910a7dab0b7cbc381e10b4
T4rp <why9ten@gmail.com>
2026-06-21 20:20:14 -0400

    feat: add matrix multiplication and matrix dumping

---
+++ gork/include/gork/tensor.h
@@ -0,0 +1,97 @@
+#ifndef GORK_TENSOR_H_
+#define GORK_TENSOR_H_
+
+#include <cassert>
+#include <cstddef>
+#include <initializer_list>
+#include <iostream>
+#include <vector>
+
+namespace gork {
+
+template <typename T> class Tensor {
+public:
+  std::vector<T> data;
+  std::vector<size_t> shape;
+  std::vector<size_t> strides;
+
+  Tensor(std::vector<size_t> &tensor_shape);
+  T &at(std::initializer_list<size_t> index);
+  void dump_mat();
+};
+
+template <typename T>
+Tensor<T>::Tensor(std::vector<size_t> &tensor_shape) : shape(tensor_shape) {
+  size_t data_size = 1;
+  for (auto dim : shape) {
+    data_size *= dim;
+  }
+
+  data.resize(data_size);
+  strides.resize(shape.size());
+
+  size_t stride = 1;
+  for (size_t i = shape.size(); i-- > 0;) {
+    strides[i] = stride;
+    stride *= shape[i];
+  }
+}
+
+template <typename T> T &Tensor<T>::at(std::initializer_list<size_t> index) {
+  assert(index.size() == shape.size());
+
+  size_t offset = 0;
+  size_t i = 0;
+
+  for (auto idx : index) {
+    offset += idx + i * strides[idx];
+    i++;
+  }
+
+  return data[offset];
+}
+
+template <typename T> void Tensor<T>::dump_mat() {
+  size_t m = shape[0];
+  size_t n = shape[1];
+
+  std::cout << "[\n";
+
+  for (size_t i = 0; i < n; i++) {
+    std::cout << "\t[";
+
+    for (size_t j = 0; j < m; j++) {
+      T val = at({i, j});
+      std::cout << val << ", ";
+    }
+
+    std::cout << "],\n";
+  }
+}
+
+template <typename T> Tensor<T> matmul(Tensor<T> &a, Tensor<T> &b) {
+  size_t m = a.shape[0];
+  size_t n = a.shape[1];
+  size_t p = b.shape[1];
+
+  std::vector<size_t> size{m, p};
+  Tensor<T> out{size};
+
+  for (size_t i = 0; i < m; i++) {
+    for (size_t j = 0; j < p; j++) {
+      T sum = 0;
+
+      for (size_t k = 0; k < n; n++) {
+        sum += a.at({i, k}) * b.at({k, j});
+      }
+
+      out.at({i, j}) = sum;
+    }
+  }
+
+  return out;
+}
+
+} // namespace gork
+
+#endif
--- testing/main.cc
+++ testing/main.cc
@@ -1,8 +1,32 @@
-#include "gork/tensors.h"
+#include "gork/tensor.h"
 #include <iostream>
+#include <random>
+#include <vector>
 
 int main() {
-  gork::Tensor<float> tensors{4, 4};
+  std::vector<size_t> shape{2, 2};
+  gork::Tensor<float> tensor1{shape};
+  gork::Tensor<float> tensor2{shape};
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<float> distr(0.0, 1.0);
+
+  tensor1.at({1, 0}) = distr(gen);
+  tensor1.at({1, 0}) = distr(gen);
+  tensor1.at({0, 1}) = distr(gen);
+  tensor1.at({1, 1}) = distr(gen);
+
+  tensor2.at({1, 0}) = distr(gen);
+  tensor2.at({1, 0}) = distr(gen);
+  tensor2.at({0, 1}) = distr(gen);
+  tensor2.at({1, 1}) = distr(gen);
+
+  tensor1.dump_mat();
+
+  // gork::Tensor<float> tensor3 = gork::matmul(tensor1, tensor2);
+
+  // tensor3.dump_mat();
 
   std::cout << "hello world\n";
   return 0;
