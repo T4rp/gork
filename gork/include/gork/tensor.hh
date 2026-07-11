dfad0ad786d6a5df2d691e8fe1337fb81be60a24
T4rp <why9ten@gmail.com>
2026-07-11 17:25:24 -0400

    fmt: change formating

--- gork/include/gork/tensor.hh
+++ gork/include/gork/tensor.hh
@@ -10,142 +10,148 @@
 
 namespace gork {
 
-template <typename T> class Tensor {
-public:
-  std::vector<T> data;
-  std::vector<size_t> shape;
-  std::vector<size_t> strides;
+template <typename T>
+class Tensor {
+  public:
+    std::vector<T> data;
+    std::vector<size_t> shape;
+    std::vector<size_t> strides;
 
-  Tensor(const std::vector<size_t> &tensor_shape);
-  T &at(std::initializer_list<size_t> index);
-  Tensor<T> clone();
+    Tensor(const std::vector<size_t> &tensor_shape);
+    T &at(std::initializer_list<size_t> index);
+    Tensor<T> clone();
 
-  void dump_mat();
+    void dump_mat();
 };
 
 template <typename T>
-Tensor<T>::Tensor(const std::vector<size_t> &tensor_shape)
-    : shape(tensor_shape) {
-  size_t data_size = 1;
-  for (auto dim : shape) {
-    data_size *= dim;
-  }
-
-  data.resize(data_size);
-  strides.resize(shape.size());
-
-  size_t stride = 1;
-  for (size_t i = shape.size(); i-- > 0;) {
-    strides[i] = stride;
-    stride *= shape[i];
-  }
-}
-
-template <typename T> T &Tensor<T>::at(std::initializer_list<size_t> index) {
-  assert(index.size() == shape.size());
-
-  size_t offset = 0;
-  size_t i = 0;
-
-  for (auto idx : index) {
-    offset += idx * strides[i];
-    i++;
-  }
-
-  return data[offset];
-}
-
-template <typename T> void Tensor<T>::dump_mat() {
-  size_t m = shape[0];
-  size_t n = shape[1];
-
-  std::cout << "[\n";
-
-  for (size_t i = 0; i < m; i++) {
-    std::cout << "\t[";
-
-    for (size_t j = 0; j < n; j++) {
-      T val = at({i, j});
-      std::cout << val << ", ";
+Tensor<T>::Tensor(const std::vector<size_t> &tensor_shape) : shape(tensor_shape) {
+    size_t data_size = 1;
+    for (auto dim : shape) {
+        data_size *= dim;
     }
 
-    std::cout << "],\n";
-  }
+    data.resize(data_size);
+    strides.resize(shape.size());
 
-  std::cout << "]\n";
-}
-
-template <typename T> Tensor<T> matmul(Tensor<T> &a, Tensor<T> &b) {
-  size_t m = a.shape[0];
-  size_t n = a.shape[1];
-  size_t p = b.shape[1];
-
-  assert(n == b.shape[0]);
-
-  std::vector<size_t> size{m, p};
-  Tensor<T> out{size};
-
-  for (size_t i = 0; i < m; i++) {
-    for (size_t j = 0; j < p; j++) {
-      T sum = 0;
-
-      for (size_t k = 0; k < n; k++) {
-        sum += a.at({i, k}) * b.at({k, j});
-      }
-
-      out.at({i, j}) = sum;
+    size_t stride = 1;
+    for (size_t i = shape.size(); i-- > 0;) {
+        strides[i] = stride;
+        stride *= shape[i];
     }
-  }
-
-  return out;
 }
 
-template <typename T> void transposeMat(Tensor<T> &mat) {
-  size_t shapeTemp{0};
-  size_t strideTemp{0};
+template <typename T>
+T &Tensor<T>::at(std::initializer_list<size_t> index) {
+    assert(index.size() == shape.size());
 
-  shapeTemp = mat.shape[0];
-  strideTemp = mat.strides[0];
+    size_t offset = 0;
+    size_t i = 0;
 
-  mat.shape[0] = mat.shape[1];
-  mat.shape[1] = shapeTemp;
-
-  mat.strides[0] = mat.strides[1];
-  mat.strides[1] = strideTemp;
-}
-
-template <typename T> Tensor<T> broadcastAdd(Tensor<T> &mat, Tensor<T> &vec) {
-  Tensor<T> tensor = mat;
-
-  size_t m = tensor.shape[0];
-  size_t n = tensor.shape[1];
-
-  assert(n == vec.shape[1]);
-  assert(vec.shape[0] == 1);
-
-  for (size_t i = 0; i < m; i++) {
-    for (size_t j = 0; j < n; j++) {
-      tensor.at({i, j}) += vec.at({0, j});
-    }
-  }
-
-  return tensor;
-}
-
-template <typename T> Tensor<T> broadcastReLU(Tensor<T> &tensor) {
-  Tensor<T> newTensor = tensor;
-
-  for (size_t i = 0; i < newTensor.data.size(); i++) {
-    T out{0.0};
-
-    if (newTensor.data[i] > 0) {
-      out = newTensor.data[i];
+    for (auto idx : index) {
+        offset += idx * strides[i];
+        i++;
     }
 
-    newTensor.data[i] = out;
-  }
+    return data[offset];
+}
 
-  return newTensor;
+template <typename T>
+void Tensor<T>::dump_mat() {
+    size_t m = shape[0];
+    size_t n = shape[1];
+
+    std::cout << "[\n";
+
+    for (size_t i = 0; i < m; i++) {
+        std::cout << "\t[";
+
+        for (size_t j = 0; j < n; j++) {
+            T val = at({i, j});
+            std::cout << val << ", ";
+        }
+
+        std::cout << "],\n";
+    }
+
+    std::cout << "]\n";
+}
+
+template <typename T>
+Tensor<T> matmul(Tensor<T> &a, Tensor<T> &b) {
+    size_t m = a.shape[0];
+    size_t n = a.shape[1];
+    size_t p = b.shape[1];
+
+    assert(n == b.shape[0]);
+
+    std::vector<size_t> size{m, p};
+    Tensor<T> out{size};
+
+    for (size_t i = 0; i < m; i++) {
+        for (size_t j = 0; j < p; j++) {
+            T sum = 0;
+
+            for (size_t k = 0; k < n; k++) {
+                sum += a.at({i, k}) * b.at({k, j});
+            }
+
+            out.at({i, j}) = sum;
+        }
+    }
+
+    return out;
+}
+
+template <typename T>
+void transposeMat(Tensor<T> &mat) {
+    size_t shapeTemp{0};
+    size_t strideTemp{0};
+
+    shapeTemp = mat.shape[0];
+    strideTemp = mat.strides[0];
+
+    mat.shape[0] = mat.shape[1];
+    mat.shape[1] = shapeTemp;
+
+    mat.strides[0] = mat.strides[1];
+    mat.strides[1] = strideTemp;
+}
+
+template <typename T>
+Tensor<T> broadcastAdd(Tensor<T> &mat, Tensor<T> &vec) {
+    Tensor<T> tensor = mat;
+
+    size_t m = tensor.shape[0];
+    size_t n = tensor.shape[1];
+
+    assert(n == vec.shape[1]);
+    assert(vec.shape[0] == 1);
+
+    for (size_t i = 0; i < m; i++) {
+        for (size_t j = 0; j < n; j++) {
+            tensor.at({i, j}) += vec.at({0, j});
+        }
+    }
+
+    return tensor;
+}
+
+template <typename T>
+Tensor<T> broadcastReLU(Tensor<T> &tensor) {
+    Tensor<T> newTensor = tensor;
+
+    for (size_t i = 0; i < newTensor.data.size(); i++) {
+        T out{0.0};
+
+        if (newTensor.data[i] > 0) {
+            out = newTensor.data[i];
+        }
+
+        newTensor.data[i] = out;
+    }
+
+    return newTensor;
 }
 
 } // namespace gork
--- testing/main.cc
+++ testing/main.cc
@@ -4,28 +4,28 @@
 #include <vector>
 
 int main() {
-  std::vector<size_t> inputShape{1, 2};
+    std::vector<size_t> inputShape{1, 2};
 
-  std::vector<size_t> layer1Shape{2, 4};
-  std::vector<size_t> layer1BiasShape{1, 4};
+    std::vector<size_t> layer1Shape{2, 4};
+    std::vector<size_t> layer1BiasShape{1, 4};
 
-  std::vector<size_t> outputShape{4, 2};
-  std::vector<size_t> outputBiasShape{1, 2};
+    std::vector<size_t> outputShape{4, 2};
+    std::vector<size_t> outputBiasShape{1, 2};
 
-  gork::Tensor<float> input{inputShape};
-  gork::Tensor<float> layer1Weights{layer1Shape};
-  gork::Tensor<float> layer1Bias{layer1BiasShape};
+    gork::Tensor<float> input{inputShape};
+    gork::Tensor<float> layer1Weights{layer1Shape};
+    gork::Tensor<float> layer1Bias{layer1BiasShape};
 
-  gork::Tensor<float> outputWeights{outputShape};
-  gork::Tensor<float> outputBias{outputBiasShape};
+    gork::Tensor<float> outputWeights{outputShape};
+    gork::Tensor<float> outputBias{outputBiasShape};
 
-  gork::Tensor<float> a = gork::matmul(input, layer1Weights);
-  gork::Tensor<float> b = gork::broadcastAdd(a, layer1Bias);
-  gork::Tensor<float> c = gork::matmul(b, outputWeights);
-  gork::Tensor<float> d = gork::broadcastAdd(c, outputBias);
+    gork::Tensor<float> a = gork::matmul(input, layer1Weights);
+    gork::Tensor<float> b = gork::broadcastAdd(a, layer1Bias);
+    gork::Tensor<float> c = gork::matmul(b, outputWeights);
+    gork::Tensor<float> d = gork::broadcastAdd(c, outputBias);
 
-  d.dump_mat();
+    d.dump_mat();
 
-  std::cout << "hello world\n";
-  return 0;
+    std::cout << "hello world\n";
+    return 0;
 }
